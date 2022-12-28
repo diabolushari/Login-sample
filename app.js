9a5c38d9d59cee914d3def2ad6aedc72d53f1899
diabolushari <harikrishnanbs2000@gmail.com>
2022-12-29 03:23:35 +0530

    Fianl

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
+const sessions = require('express-session');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -16,9 +17,18 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// Validity of session
+const oneMin = 60000;
+// session create middleware
+app.use(sessions({
+  secret: "mysecret",
+  saveUninitialized:true,
+  cookie: { maxAge: oneMin },
+  resave: false
+}));
 
+app.use(cookieParser());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
